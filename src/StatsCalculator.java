de7f1fafd7738b9933155d5ced983b07578cb611
Dylan Nguyen <nguyend@wlhs.wlwv.k12.or.us>
2023-03-09 09:47:55 -0800

    beuh

--- src/StatsCalculator.java
+++ src/StatsCalculator.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
 public class StatsCalculator {
-    private static final double[] values = {-5.5, 10.7, 0.0, -3.8, 1.2, 4.3, -2.5, 1.2, 7.0, -0.5};
-    private static double[] sortedValues = new double[]{10};
-    sortedValues = values;
-
+    public static void main(String[] args) {
+        final double[] values = {-5.5, 10.7, 0.0, -3.8, 1.2, 4.3, -2.5, 1.2, 7.0, -0.5};
+        Arrays.sort(values);
     }
+    private static double[] sortedValues = new double[10];
+
+
 }
