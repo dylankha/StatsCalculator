b1cbbb2c0872b4abfe88536afb4893ae8942fe09
Dylan Nguyen <nguyend@wlhs.wlwv.k12.or.us>
2023-03-15 08:58:06 -0700

    beuh

--- src/StatsCalculator.java
+++ src/StatsCalculator.java
@@ -1,10 +1,52 @@
 import java.util.Arrays;
+
 public class StatsCalculator {
-    public static void main(String[] args) {
-        final double[] values = {-5.5, 10.7, 0.0, -3.8, 1.2, 4.3, -2.5, 1.2, 7.0, -0.5};
-        Arrays.sort(values);
+    private double [] values =  {10.0, 20.0, 5.0, 15.0, 25.0, 30.0, 8.0};
+    private double [] sortedValues;
+
+    /**
+     *  default constructor makes an array of size 20 full of 0s
+     */
+    public StatsCalculator(){
+        this.sortedValues = new double[20];
     }
-    private static double[] sortedValues = new double[10];
+
+    /**
+     * Overloaded constructor that sets instance variable equal to the parameter
+     * @param values
+     */
+    public StatsCalculator(double[] values){
+        this.sortedValues = values;
+    }
+
+    /**
+     * Sorts the sortedValues array
+     */
+    public void sortData(){
+        Arrays.sort(sortedValues);
+    }
+
+    public double calculateMax(){
+        double max = 0;
+        for (int i = 1; i < sortedValues.length; i++){
+            if (sortedValues[i] > sortedValues[i-1]){
+                max = sortedValues[i];
+            }
+        }
+        return max;
+    }
+
+    public double calculateMin(){
+        double min = 0;
+        for (int i = 1; i < sortedValues.length; i++){
+            if (sortedValues[i] < sortedValues[i-1]){
+                min = sortedValues[i];
+            }
+        }
+        return min;
+    }
+
+
 
 
 }
